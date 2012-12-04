fcdee47db3579b62cee361ea05925b63f9415b29
Sam Coward <pair+scoward@pivotallabs.com>
2012-12-04 18:32:13 -0500

    Initial Commit

---
+++ Karrabing/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Karrabing
+//
+//  Created by pivotal on 12/4/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
